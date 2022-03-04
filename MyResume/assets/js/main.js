1e9d449fefad3c13e7034a7e4134bc37cc3e4c91
Matías Rodriguez <matiasemanuelr98@gmail.com>
2022-03-04 01:21:24 -0300

    new web

--- MyResume/assets/js/main.js
+++ MyResume/assets/js/main.js
@@ -145,9 +145,9 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 30,
-      backSpeed: 30,
-      backDelay: 1000
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
     });
   }
 
