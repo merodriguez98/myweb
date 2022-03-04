85133684db7508454b4182f04eaaaec1c408a05a
Matías Rodriguez <matiasemanuelr98@gmail.com>
2022-03-04 01:23:10 -0300

    delete comentarios, img

--- MyResume/assets/js/main.js
+++ MyResume/assets/js/main.js
@@ -145,9 +145,9 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
+      typeSpeed: 40,
+      backSpeed: 30,
+      backDelay: 1000
     });
   }
 
